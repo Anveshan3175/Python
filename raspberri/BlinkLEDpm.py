2d18d94d4a9e8d7a21680d54ff91c0ca95e631a5
Anveshan <akunduru4@gmail.com>
2017-07-11 18:30:18 +0530

    Update BlinkLEDpm.py

--- raspberri/BlinkLEDpm.py
+++ raspberri/BlinkLEDpm.py
@@ -5,7 +5,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(19, GPIO.OUT)
 
-p = GPIO.PWM(12, 0.5)
+p = GPIO.PWM(19, 0.5)
 p.start(1)
 input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
